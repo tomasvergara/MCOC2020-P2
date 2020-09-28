3cdc55998dffc8c93a168bbf848a777a96607d92
JulioGalindoC <69281523+JulioGalindoC@users.noreply.github.com>
2020-09-28 17:26:57 -0300

    Update reticulado.py

--- reticulado.py
+++ reticulado.py
@@ -2,7 +2,6 @@ import numpy as np
 
 class Reticulado(object):
 	"""Define un reticulado"""
-
 	def __init__(self):
 		super(Reticulado, self).__init__()
 		
@@ -12,33 +11,31 @@ class Reticulado(object):
 		self.cargas = {}
 		self.restricciones = {}
 
-	def agregar_nodo(self, x, y, z=0):
-		#Cambiar Tamaño
-		self.xyz.resize((self.Nnodos+1,3))
-		self.xyz[self.Nnodos,:] = [x,y,z]
-		self.Nnodos +=1
-		return
+    	def agregar_nodo(self, x, y, z=0):
+        	self.xyz.resize((self.Nnodos+1,3))
+        	self.xyz[self.Nnodos,:] = [x,y,z]
+        	self.Nnodos += 1
+        	return
 		
 	def agregar_barra(self, barra):
-		"""Implementar"""
-		return
-
+		return self.barras.append(barra)
+		
 	def obtener_coordenada_nodal(self, n): 
-		"""Implementar"""
-		return 
-
+		if n >= self.Nnodos :
+			return
+		return self.xyz[n, :]
+		
 	def calcular_peso_total(self):
-		"""Implementar"""
-		return 
-
+		peso = 0
+        	for barra in self.barras :
+            		peso += barra.calcular_peso(self)  
+        	return peso
+		
 	def obtener_nodos(self):
-		"""Implementar"""
-
-		return 
-
+		return self.xyz 
+		
 	def obtener_barras(self):
-		"""Implementar"""
-		return 
+		return self.barras 
 
 	def agregar_restriccion(self, nodo, gdl, valor=0.0):
 		"""Implementar"""
@@ -60,9 +57,15 @@ class Reticulado(object):
 		"""Implementar"""
 		return
 
-	def __str__(self):
-		s = "Hola soy un reticulado!\n"
-		s += "mis nodos son:"
-		s += f"{self.xyz}"
-		return s
-
+    	def __str__(self):
+        	s = "nodos:\n"
+        	for n in range(self.Nnodos) :
+            		s += f" {n} : ( {self.xyz[n][0]}, {self.xyz[n][1]}, {self.xyz[n][2]})\n"
+        	s += "\n\n"
+        	s += "barras:\n"
+        	
+        	for i, b in enumerate(self.barras) :
+            		n = b.obtener_conectividad()
+            		s += f" {i} : [ {n[0]} {n[1]} ] \n"
+        	return s
+	
